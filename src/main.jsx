665d72b3a85f007b4508154aa708183c04db339f
michal <m.lubinski@student.maastrichtuniversity.nl>
2025-06-28 19:58:48 +0200

    Initial working version

---
+++ src/main.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Hi, I am Micheal and this is my website about me</h1>
+    </div>
+  );
+}
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
