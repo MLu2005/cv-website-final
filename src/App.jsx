8592b9edbaa24f90769e8afb5c36e23ac05d18f1
michal <m.lubinski@student.maastrichtuniversity.nl>
2025-06-28 21:36:57 +0200

    Add App.jsx main component

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '40vh' }}>
+      <h1>Hi, I am Micheal and this is my website about me</h1>
+    </div>
+  );
+}
+
+export default App;
